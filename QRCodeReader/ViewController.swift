67803463615f7bff473c0d3ebfab461631a33809
Timur Saidov <timur.saidov@ramax.ru>
2019-12-25 12:37:31 +0300

    Initial Commit

---
+++ QRCodeReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCodeReader
+//
+//  Created by Timur Saidov on 25/12/2019.
+//  Copyright © 2019 Timur Saidov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
